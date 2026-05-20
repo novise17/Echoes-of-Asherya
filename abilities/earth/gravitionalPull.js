2c9d7f7cbef5e0c5aa7192df46b439d1f1a928ac
novise17 <koffin@nycstudents.net>
2026-05-20 11:39:54 -0400

    Update gravitionalPull.js

--- abilities/earth/gravitionalPull.js
+++ abilities/earth/gravitionalPull.js
@@ -1,1 +1,14 @@
+import { Ability } from "../Ability.js";
 
+export class GravitonPull extends Ability {
+    constructor() {
+        super({ name: "Graviton Pull", cooldown: 1800, energyCost: 20 });
+    }
+
+    activate(fighter, enemy) {
+        const direction = fighter.x < enemy.x ? -1 : 1;
+
+        enemy.x -= direction * 60;
+        enemy.health -= 6;
+    }
+}
