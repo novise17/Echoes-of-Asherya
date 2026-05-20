c4c74f5a366fa3b538b8f719c868b0ca8518dc4f
novise17 <koffin@nycstudents.net>
2026-05-20 11:45:41 -0400

    Update absoluteZero.js

--- abilities/ice/absoluteZero.js
+++ abilities/ice/absoluteZero.js
@@ -1,1 +1,16 @@
+import { Ability } from "../Ability.js";
 
+export class AbsoluteZero extends Ability {
+    constructor() {
+        super({ name: "Absolute Zero", cooldown: 8000, energyCost: 60 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 38;
+        enemy.speed = 1;
+
+        setTimeout(() => {
+            enemy.speed = 6;
+        }, 3000);
+    }
+}
