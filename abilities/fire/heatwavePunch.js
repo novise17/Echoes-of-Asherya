ae6fc188582f1803abf5d1d78df88f32a7ae5333
novise17 <koffin@nycstudents.net>
2026-05-20 11:33:46 -0400

    Update heatwavePunch.js

--- abilities/fire/heatwavePunch.js
+++ abilities/fire/heatwavePunch.js
@@ -1,1 +1,17 @@
+import { Ability } from "../Ability.js";
 
+export class HeatwavePunch extends Ability {
+    constructor() {
+        super({
+            name: "Heatwave Punch",
+            cooldown: 1200,
+            energyCost: 15
+        });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 18;
+
+        enemy.x += fighter.facing * 30; // knockback
+    }
+}
