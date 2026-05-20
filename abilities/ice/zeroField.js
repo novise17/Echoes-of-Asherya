4d3cbad214bea81feb66ffa824e6ae5974866351
novise17 <koffin@nycstudents.net>
2026-05-20 11:44:41 -0400

    Update zeroField.js

--- abilities/ice/zeroField.js
+++ abilities/ice/zeroField.js
@@ -1,1 +1,15 @@
+import { Ability } from "../Ability.js";
 
+export class ZeroField extends Ability {
+    constructor() {
+        super({ name: "Zero Field", cooldown: 2000, energyCost: 20 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.speed = 2;
+
+        setTimeout(() => {
+            enemy.speed = 6;
+        }, 2500);
+    }
+}
