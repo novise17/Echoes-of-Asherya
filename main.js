7c4a0666efcb85eb0a0bb8bc675acfa43ef1f8f3
novise17 <koffin@nycstudents.net>
2026-05-20 11:35:38 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,78 +1,76 @@
 import { Fighter } from "./fighters/Fighter.js";
 
-// -------------------- CANVAS --------------------
+import { PlasmaDash } from "./abilities/fire/plasmaDash.js";
+import { HeatwavePunch } from "./abilities/fire/heatwavePunch.js";
+import { IgnitionField } from "./abilities/fire/ignitionfield.js";
+import { FirestormBarrage } from "./abilities/fire/firestormBarrage.js";
+import { SolarCollapse } from "./abilities/fire/solarCollapse.js";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 1200;
 canvas.height = 600;
 
-// -------------------- INPUT SYSTEM --------------------
 const keys = {};
 
+window.addEventListener("keydown", (e) => keys[e.key] = true);
+window.addEventListener("keyup", (e) => keys[e.key] = false);
+
+// PLAYERS
+const player1 = new Fighter("Blaze", 200, "red", {
+    left: "a",
+    right: "d",
+    jump: "w",
+    attack: " "
+});
+
+const player2 = new Fighter("Volt", 900, "blue", {
+    left: "ArrowLeft",
+    right: "ArrowRight",
+    jump: "ArrowUp",
+    attack: "Enter"
+});
+
+// ASSIGN FIRE ABILITIES TO PLAYER 1
+player1.abilities = {
+    dash: new PlasmaDash(),
+    punch: new HeatwavePunch(),
+    field: new IgnitionField(),
+    barrage: new FirestormBarrage(),
+    ultimate: new SolarCollapse()
+};
+
+// INPUT ABILITIES
 window.addEventListener("keydown", (e) => {
-    keys[e.key] = true;
+    if (e.key === "q") player1.abilities.dash.use(player1, player2);
+    if (e.key === "e") player1.abilities.punch.use(player1, player2);
+    if (e.key === "r") player1.abilities.field.use(player1, player2);
+    if (e.key === "f") player1.abilities.barrage.use(player1, player2);
+    if (e.key === "g") player1.abilities.ultimate.use(player1, player2);
 });
 
-window.addEventListener("keyup", (e) => {
-    keys[e.key] = false;
-});
-
-// -------------------- FIGHTERS --------------------
-const player1 = new Fighter(
-    "Blaze",
-    200,
-    "red",
-    {
-        left: "a",
-        right: "d",
-        jump: "w",
-        attack: " "
-    }
-);
-
-const player2 = new Fighter(
-    "Volt",
-    900,
-    "blue",
-    {
-        left: "ArrowLeft",
-        right: "ArrowRight",
-        jump: "ArrowUp",
-        attack: "Enter"
-    }
-);
-
-// -------------------- GAME LOOP --------------------
 function update() {
     player1.move(keys, canvas);
     player2.move(keys, canvas);
 
-    // attacks
-    if (keys[player1.controls.attack]) {
-        player1.attack(player2);
-    }
-
-    if (keys[player2.controls.attack]) {
-        player2.attack(player1);
-    }
+    if (keys[player1.controls.attack]) player1.attack(player2);
+    if (keys[player2.controls.attack]) player2.attack(player1);
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // arena floor
-    ctx.fillStyle = "#333";
-    ctx.fillRect(0, canvas.height - 20, canvas.width, 20);
-
     player1.draw(ctx);
     player2.draw(ctx);
+
+    player1.updateProjectiles(ctx, player2);
+    player2.updateProjectiles(ctx, player1);
 
     drawUI();
 }
 
 function drawUI() {
-    // health bars
     ctx.fillStyle = "red";
     ctx.fillRect(20, 20, player1.health * 3, 20);
 
