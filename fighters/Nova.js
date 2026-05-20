dcc2b048fbb18145be0ed7110131ce908367dade
novise17 <koffin@nycstudents.net>
2026-05-20 11:47:04 -0400

    Update Nova.js

--- fighters/Nova.js
+++ fighters/Nova.js
@@ -1,1 +1,23 @@
+import { Fighter } from "./Fighter.js";
 
+import { PulsarBeam } from "../abilities/cosmic/pulsarBeam.js";
+import { GravityRift } from "../abilities/cosmic/gravityRift.js";
+import { NebulaBurst } from "../abilities/cosmic/nebulaBurst.js";
+import { OrbitShift } from "../abilities/cosmic/orbitShift.js";
+import { SupernovaAscension } from "../abilities/cosmic/supernovaAscension.js";
+
+export class Nova extends Fighter {
+    constructor(x, color, controls) {
+        super("Nova", x, color, controls);
+
+        this.abilities = {
+            q: new PulsarBeam(),
+            w: new GravityRift(),
+            e: new NebulaBurst(),
+            r: new OrbitShift(),
+            f: new SupernovaAscension()
+        };
+
+        this.cosmicCharge = 0;
+    }
+}
