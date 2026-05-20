6b321e4fa8f2ecca7b35255dfb07fea97d1f971e
novise17 <koffin@nycstudents.net>
2026-05-20 11:34:27 -0400

    Update firestormBarrage.js

--- abilities/fire/firestormBarrage.js
+++ abilities/fire/firestormBarrage.js
@@ -1,1 +1,20 @@
+import { Ability } from "../Ability.js";
 
+export class FirestormBarrage extends Ability {
+    constructor() {
+        super({
+            name: "Firestorm Barrage",
+            cooldown: 2500,
+            energyCost: 30
+        });
+    }
+
+    activate(fighter, enemy) {
+        for (let i = 0; i < 5; i++) {
+            setTimeout(() => {
+                enemy.health -= 4;
+                enemy.x += fighter.facing * 5;
+            }, i * 200);
+        }
+    }
+}
