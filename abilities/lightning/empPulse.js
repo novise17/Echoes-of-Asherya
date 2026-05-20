15a0bc0de3a7583459ec00220ab28b306c152847
novise17 <koffin@nycstudents.net>
2026-05-20 11:42:38 -0400

    Update empPulse.js

--- abilities/lightning/empPulse.js
+++ abilities/lightning/empPulse.js
@@ -1,1 +1,12 @@
+import { Ability } from "../Ability.js";
 
+export class EMPPulse extends Ability {
+    constructor() {
+        super({ name: "EMP Pulse", cooldown: 2000, energyCost: 20 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.energy -= 30;
+        enemy.health -= 5;
+    }
+}
