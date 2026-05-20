eda0b5a94ebbeffc1f85dd60ebd1b5d09cb3a94c
novise17 <koffin@nycstudents.net>
2026-05-20 11:43:23 -0400

    Update thunderGodProtocol.js

--- abilities/lightning/thunderGodProtocol.js
+++ abilities/lightning/thunderGodProtocol.js
@@ -1,1 +1,18 @@
+import { Ability } from "../Ability.js";
 
+export class ThunderGodProtocol extends Ability {
+    constructor() {
+        super({ name: "Thunder God Protocol", cooldown: 8000, energyCost: 60 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 40;
+        enemy.x += fighter.facing * 120;
+
+        fighter.speed += 3;
+
+        setTimeout(() => {
+            fighter.speed -= 3;
+        }, 3000);
+    }
+}
