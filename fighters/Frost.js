e368017a56107e26bf3b6c2e39ff4241409add99
novise17 <koffin@nycstudents.net>
2026-05-20 11:43:52 -0400

    Update Frost.js

--- fighters/Frost.js
+++ fighters/Frost.js
@@ -1,1 +1,21 @@
+import { Fighter } from "./Fighter.js";
 
+import { CryoLance } from "../abilities/ice/cryoLance.js";
+import { ZeroField } from "../abilities/ice/zeroField.js";
+import { CrystalWall } from "../abilities/ice/crystalWall.js";
+import { PermafrostChains } from "../abilities/ice/permafrostChains.js";
+import { AbsoluteZero } from "../abilities/ice/absoluteZero.js";
+
+export class Frost extends Fighter {
+    constructor(x, color, controls) {
+        super("Frost", x, color, controls);
+
+        this.abilities = {
+            q: new CryoLance(),
+            w: new ZeroField(),
+            e: new CrystalWall(),
+            r: new PermafrostChains(),
+            f: new AbsoluteZero()
+        };
+    }
+}
