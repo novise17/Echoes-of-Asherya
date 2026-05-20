13859ff2af5f694ffe873d94851240d5dc57651b
novise17 <koffin@nycstudents.net>
2026-05-20 11:47:56 -0400

    Update gravityRift.js

--- abilities/cosmic/gravityRift.js
+++ abilities/cosmic/gravityRift.js
@@ -1,1 +1,13 @@
+import { Ability } from "../Ability.js";
 
+export class GravityRift extends Ability {
+    constructor() {
+        super({ name: "Gravity Rift", cooldown: 2000, energyCost: 20 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.x += fighter.facing * 60;
+        enemy.y -= 20;
+        enemy.health -= 8;
+    }
+}
