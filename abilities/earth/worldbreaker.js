6035587a483409f77d98daaa1d48976f64fc9516
novise17 <koffin@nycstudents.net>
2026-05-20 11:40:56 -0400

    Update worldbreaker.js

--- abilities/earth/worldbreaker.js
+++ abilities/earth/worldbreaker.js
@@ -1,1 +1,19 @@
+import { Ability } from "../Ability.js";
 
+export class Worldbreaker extends Ability {
+    constructor() {
+        super({ name: "Worldbreaker", cooldown: 8000, energyCost: 60 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 35;
+        enemy.x += fighter.facing * 150;
+
+        // temporary slow effect
+        enemy.speed = 2;
+
+        setTimeout(() => {
+            enemy.speed = 6;
+        }, 3000);
+    }
+}
