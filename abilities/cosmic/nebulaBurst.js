d53333762730a1cb850607d2510b45c3c9f87434
novise17 <koffin@nycstudents.net>
2026-05-20 11:48:11 -0400

    Update nebulaBurst.js

--- abilities/cosmic/nebulaBurst.js
+++ abilities/cosmic/nebulaBurst.js
@@ -1,1 +1,14 @@
+import { Ability } from "../Ability.js";
 
+export class NebulaBurst extends Ability {
+    constructor() {
+        super({ name: "Nebula Burst", cooldown: 1800, energyCost: 18 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 10;
+
+        // soft push
+        enemy.x += fighter.facing * 30;
+    }
+}
