3d70dbe0a9d6bbdbdc4095df8fa4ea1f8bf5255c
novise17 <koffin@nycstudents.net>
2026-05-20 11:34:52 -0400

    Update solarCollapse.js

--- abilities/fire/solarCollapse.js
+++ abilities/fire/solarCollapse.js
@@ -1,1 +1,25 @@
+import { Ability } from "../Ability.js";
 
+export class SolarCollapse extends Ability {
+    constructor() {
+        super({
+            name: "Solar Collapse",
+            cooldown: 8000,
+            energyCost: 60
+        });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 35;
+
+        // massive knockback
+        enemy.x += fighter.facing * 120;
+
+        // temporary arena pressure effect
+        fighter.speed += 2;
+
+        setTimeout(() => {
+            fighter.speed -= 2;
+        }, 3000);
+    }
+}
