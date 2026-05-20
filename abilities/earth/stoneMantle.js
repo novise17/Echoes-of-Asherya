0bc08669ccc063de84532e8ce337a255c7433979
novise17 <koffin@nycstudents.net>
2026-05-20 11:40:20 -0400

    Update stoneMantle.js

--- abilities/earth/stoneMantle.js
+++ abilities/earth/stoneMantle.js
@@ -1,1 +1,15 @@
+import { Ability } from "../Ability.js";
 
+export class StoneMantle extends Ability {
+    constructor() {
+        super({ name: "Stone Mantle", cooldown: 2500, energyCost: 25 });
+    }
+
+    activate(fighter) {
+        fighter.armor = 30;
+
+        setTimeout(() => {
+            fighter.armor = 0;
+        }, 4000);
+    }
+}
