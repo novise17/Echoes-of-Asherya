f5f0e4def303e0050985e7573b6844dbe7b0fc74
novise17 <koffin@nycstudents.net>
2026-05-20 11:29:10 -0400

    Update Fighter.js

--- fighters/Fighter.js
+++ fighters/Fighter.js
@@ -9,21 +9,26 @@ export class Fighter {
         this.height = 120;
 
         this.velocityY = 0;
+        this.gravity = 0.7;
+
         this.speed = 6;
         this.jumpForce = -15;
-
-        this.gravity = 0.7;
 
         this.color = color;
 
         this.health = 100;
 
+        this.energy = 100;
+        this.maxEnergy = 100;
+
         this.isGrounded = false;
         this.isAttacking = false;
 
+        this.facing = 1;
+
         this.controls = controls;
 
-        this.facing = 1; // 1 = right, -1 = left
+        this.projectiles = [];
     }
 
     move(keys, canvas) {
@@ -42,15 +47,22 @@ export class Fighter {
             this.isGrounded = false;
         }
 
-        // apply gravity
+        // gravity
         this.y += this.velocityY;
         this.velocityY += this.gravity;
 
-        // floor collision
         if (this.y + this.height >= canvas.height) {
             this.y = canvas.height - this.height;
             this.velocityY = 0;
             this.isGrounded = true;
+        }
+
+        this.regenEnergy();
+    }
+
+    regenEnergy() {
+        if (this.energy < this.maxEnergy) {
+            this.energy += 0.2;
         }
     }
 
@@ -80,11 +92,37 @@ export class Fighter {
         }, 150);
     }
 
+    updateProjectiles(ctx, enemy) {
+        for (let i = 0; i < this.projectiles.length; i++) {
+            let p = this.projectiles[i];
+
+            p.x += p.speed;
+
+            ctx.fillStyle = p.color || "orange";
+            ctx.fillRect(p.x, p.y, p.width, p.height);
+
+            if (
+                p.x < enemy.x + enemy.width &&
+                p.x + p.width > enemy.x &&
+                p.y < enemy.y + enemy.height &&
+                p.y + p.height > enemy.y
+            ) {
+                enemy.health -= p.damage;
+                this.projectiles.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
     draw(ctx) {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
 
-        // optional hitbox debug
+        // energy bar (simple)
+        ctx.fillStyle = "cyan";
+        ctx.fillRect(this.x, this.y - 10, this.energy, 5);
+
+        // attack debug
         if (this.isAttacking) {
             ctx.fillStyle = "yellow";
             ctx.fillRect(
