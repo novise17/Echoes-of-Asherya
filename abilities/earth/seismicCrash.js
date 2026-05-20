64994ac013270f8c95c38b46d8c044ac4a2e0159
novise17 <koffin@nycstudents.net>
2026-05-20 11:39:35 -0400

    Update seismicCrash.js

--- abilities/earth/seismicCrash.js
+++ abilities/earth/seismicCrash.js
@@ -1,1 +1,13 @@
+import { Ability } from "../Ability.js";
 
+export class SeismicCrash extends Ability {
+    constructor() {
+        super({ name: "Seismic Crash", cooldown: 1200, energyCost: 15 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 14;
+        enemy.y -= 20;
+        enemy.x += fighter.facing * 10;
+    }
+}
