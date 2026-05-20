6ce869bbec61329363f0befe16794febd8df8178
novise17 <koffin@nycstudents.net>
2026-05-20 11:33:07 -0400

    Update plasmaDash.js

--- abilities/fire/plasmaDash.js
+++ abilities/fire/plasmaDash.js
@@ -1,1 +1,20 @@
+import { Ability } from "../Ability.js";
 
+export class PlasmaDash extends Ability {
+    constructor() {
+        super({
+            name: "Plasma Dash",
+            cooldown: 800,
+            energyCost: 12
+        });
+    }
+
+    activate(fighter, enemy) {
+        fighter.x += fighter.facing * 80;
+        fighter.isAttacking = true;
+
+        setTimeout(() => {
+            fighter.isAttacking = false;
+        }, 100);
+    }
+}
