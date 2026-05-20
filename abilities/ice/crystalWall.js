2be947882a9927531dba2c8dd57459c5bf4a4770
novise17 <koffin@nycstudents.net>
2026-05-20 11:45:09 -0400

    Update crystalWall.js

--- abilities/ice/crystalWall.js
+++ abilities/ice/crystalWall.js
@@ -1,1 +1,15 @@
+import { Ability } from "../Ability.js";
 
+export class CrystalWall extends Ability {
+    constructor() {
+        super({ name: "Crystal Wall", cooldown: 1800, energyCost: 18 });
+    }
+
+    activate(fighter) {
+        fighter.armor = 25;
+
+        setTimeout(() => {
+            fighter.armor = 0;
+        }, 3000);
+    }
+}
