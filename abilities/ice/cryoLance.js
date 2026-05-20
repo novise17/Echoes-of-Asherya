c56cf03dbfd2bc30f341dc14e901a92c6d5f0e7e
novise17 <koffin@nycstudents.net>
2026-05-20 11:44:21 -0400

    Update cryoLance.js

--- abilities/ice/cryoLance.js
+++ abilities/ice/cryoLance.js
@@ -1,1 +1,11 @@
+import { Ability } from "../Ability.js";
 
+export class CryoLance extends Ability {
+    constructor() {
+        super({ name: "Cryo Lance", cooldown: 900, energyCost: 12 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 10;
+    }
+}
