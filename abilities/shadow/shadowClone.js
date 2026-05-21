5ddcaa035a3b1c9ca4698a84bdc626597cfd496e
novise17 <koffin@nycstudents.net>
2026-05-21 10:03:41 -0400

    Update shadowClone.js

--- abilities/shadow/shadowClone.js
+++ abilities/shadow/shadowClone.js
@@ -1,1 +1,14 @@
+import { Ability } from "../Ability.js";
 
+export class ShadowClone extends Ability {
+    constructor() {
+        super({ name: "Shadow Clone", cooldown: 1500, energyCost: 18 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 6;
+        enemy.x += fighter.facing * 20;
+
+        // fake pressure effect
+    }
+}
