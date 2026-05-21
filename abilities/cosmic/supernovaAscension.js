7c9dee368052effd9580969f5f6a4e1ab2034138
novise17 <koffin@nycstudents.net>
2026-05-21 10:00:57 -0400

    Update supernovaAscension.js

--- abilities/cosmic/supernovaAscension.js
+++ abilities/cosmic/supernovaAscension.js
@@ -1,1 +1,19 @@
+import { Ability } from "../Ability.js";
 
+export class SupernovaAscension extends Ability {
+    constructor() {
+        super({ name: "Supernova Ascension", cooldown: 8000, energyCost: 60 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 40;
+        enemy.x += fighter.facing * 140;
+
+        // temporary power boost
+        fighter.speed += 2;
+
+        setTimeout(() => {
+            fighter.speed -= 2;
+        }, 3000);
+    }
+}
