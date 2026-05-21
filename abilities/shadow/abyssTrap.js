d070f38445ed528dcec30c1f247e087dbe0c331d
novise17 <koffin@nycstudents.net>
2026-05-21 10:04:24 -0400

    Update abyssTrap.js

--- abilities/shadow/abyssTrap.js
+++ abilities/shadow/abyssTrap.js
@@ -1,1 +1,15 @@
+import { Ability } from "../Ability.js";
 
+export class AbyssTrap extends Ability {
+    constructor() {
+        super({ name: "Abyss Trap", cooldown: 2200, energyCost: 22 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.speed = 1;
+
+        setTimeout(() => {
+            enemy.speed = 6;
+        }, 2500);
+    }
+}
