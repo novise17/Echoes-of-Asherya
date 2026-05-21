7969c1246a237e87bbc040da20e525e7be02bf70
novise17 <koffin@nycstudents.net>
2026-05-21 10:17:12 -0400

    Update screenShake.js

--- assets/vfx/screenShake.js
+++ assets/vfx/screenShake.js
@@ -1,1 +1,21 @@
+export let screenShake = 0;
 
+export function triggerShake(power) {
+    screenShake = power;
+}
+
+export function applyShake(ctx) {
+
+    if (screenShake <= 0) return;
+
+    const dx = (Math.random() - 0.5) * screenShake;
+    const dy = (Math.random() - 0.5) * screenShake;
+
+    ctx.translate(dx, dy);
+
+    screenShake *= 0.9;
+
+    if (screenShake < 0.5) {
+        screenShake = 0;
+    }
+}
