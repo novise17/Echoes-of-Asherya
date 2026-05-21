c9392ea405c45d3b5e42918abcaafa1c77c00553
novise17 <koffin@nycstudents.net>
2026-05-21 10:05:06 -0400

    Update eclipseDomain.js

--- abilities/shadow/eclipseDomain.js
+++ abilities/shadow/eclipseDomain.js
@@ -1,1 +1,18 @@
+import { Ability } from "../Ability.js";
 
+export class EclipseDomain extends Ability {
+    constructor() {
+        super({ name: "Eclipse Domain", cooldown: 8000, energyCost: 60 });
+    }
+
+    activate(fighter, enemy) {
+        enemy.health -= 35;
+
+        // disorient effect (simple version)
+        enemy.speed = 2;
+
+        setTimeout(() => {
+            enemy.speed = 6;
+        }, 3000);
+    }
+}
