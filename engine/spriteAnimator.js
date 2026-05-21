719fd92093fe792189595e7b197bdc37c1c0d502
novise17 <koffin@nycstudents.net>
2026-05-21 11:19:21 -0400

    Create spriteAnimator.js

---
+++ engine/spriteAnimator.js
@@ -0,0 +1,63 @@
+// ============================================
+// FILE: engine/SpriteAnimator.js
+// ============================================
+
+export class SpriteAnimator {
+
+    constructor(image, frameWidth, frameHeight, frameCount, fps = 10) {
+
+        this.image = image;
+
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+
+        this.frameCount = frameCount;
+
+        this.currentFrame = 0;
+
+        this.fps = fps;
+        this.tick = 0;
+    }
+
+    update() {
+
+        this.tick++;
+
+        if (this.tick >= this.fps) {
+
+            this.tick = 0;
+
+            this.currentFrame++;
+
+            if (this.currentFrame >= this.frameCount) {
+
+                this.currentFrame = 0;
+            }
+        }
+    }
+
+    draw(ctx, x, y, width, height, flip = false) {
+
+        ctx.save();
+
+        if (flip) {
+
+            ctx.scale(-1, 1);
+            x = -x - width;
+        }
+
+        ctx.drawImage(
+            this.image,
+            this.currentFrame * this.frameWidth,
+            0,
+            this.frameWidth,
+            this.frameHeight,
+            x,
+            y,
+            width,
+            height
+        );
+
+        ctx.restore();
+    }
+}
