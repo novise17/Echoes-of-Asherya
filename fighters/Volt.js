86d4a3956eb0a6bd7e39a5df2a574687560c513c
novise17 <koffin@nycstudents.net>
2026-05-21 11:00:27 -0400

    Update Volt.js

--- fighters/Volt.js
+++ fighters/Volt.js
@@ -1,23 +1,42 @@
+// ============================================
+// FILE: fighters/Volt.js
+// ============================================
+
 import { Fighter } from "./Fighter.js";
 
-import { Railstep } from "../abilities/lightning/railstep.js";
-import { ArcKnives } from "../abilities/lightning/arcKnives.js";
-import { EMPPulse } from "../abilities/lightning/empPulse.js";
-import { StaticPrison } from "../abilities/lightning/staticPrison.js";
-import { ThunderGodProtocol } from "../abilities/lightning/thunderGodProtocol.js";
+import { lightningDash } from "../abilities/lightningDash.js";
+import { railJab } from "../abilities/railJab.js";
+import { staticMinefield } from "../abilities/staticMinefield.js";
+import { thunderChain } from "../abilities/thunderChain.js";
+import { stormOverclock } from "../abilities/stormOverclock.js";
 
 export class Volt extends Fighter {
-    constructor(x, color, controls) {
-        super("Volt", x, color, controls);
 
-        this.abilities = {
-            q: new Railstep(),
-            w: new ArcKnives(),
-            e: new EMPPulse(),
-            r: new StaticPrison(),
-            f: new ThunderGodProtocol()
-        };
+    constructor(x, controls) {
 
-        this.overcharged = false;
+        super("Volt", x, "blue", controls);
+
+        this.nodes = 0;
+
+        this.abilities = [
+            lightningDash,
+            railJab,
+            staticMinefield,
+            thunderChain,
+            stormOverclock
+        ];
+    }
+
+    onHit(enemy) {
+
+        // ⚡ NODE SYSTEM
+        this.nodes++;
+
+        if (this.nodes >= 3) {
+
+            this.energy = Math.min(this.maxEnergy, this.energy + 25);
+
+            this.nodes = 0;
+        }
     }
 }
