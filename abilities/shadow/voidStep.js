d86fda8bbf8dc75a6ca8afd6523101d4c98fe68e
novise17 <koffin@nycstudents.net>
2026-05-21 10:03:24 -0400

    Update voidStep.js

--- abilities/shadow/voidStep.js
+++ abilities/shadow/voidStep.js
@@ -1,1 +1,11 @@
+import { Ability } from "../Ability.js";
 
+export class VoidStep extends Ability {
+    constructor() {
+        super({ name: "Void Step", cooldown: 600, energyCost: 10 });
+    }
+
+    activate(fighter) {
+        fighter.x += fighter.facing * 90;
+    }
+}
